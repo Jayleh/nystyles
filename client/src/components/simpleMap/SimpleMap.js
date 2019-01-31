acd97e647f4aa43fc8b767d48da65a86224b446f
Jayleh <jaylehyun@gmail.com>
2019-01-30 22:45:28 -0800

    scripted function to get google maps key from env variables

--- client/src/components/simpleMap/SimpleMap.js
+++ client/src/components/simpleMap/SimpleMap.js
@@ -7,9 +7,17 @@ const SimpleMap = ({ lat, lng }) => {
     mapTypeControl: true
   };
 
+  const aquireGoogleMapsKey = () => {
+    if (process.env.NODE_ENV === 'development') {
+      return process.env.REACT_APP_GOOGLE_MAPS_KEY;
+    } else if (process.env.NODE_ENV === 'production') {
+      return process.env.GOOGLE_MAPS_KEY;
+    }
+  };
+
   return (
     <GoogleMapReact
-      bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAPS_KEY }}
+      bootstrapURLKeys={{ key: aquireGoogleMapsKey() }}
       defaultCenter={{ lat, lng }}
       defaultZoom={14}
       options={options}
