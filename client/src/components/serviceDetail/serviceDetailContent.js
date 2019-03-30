b1feb619bbba2fb5cc90168c9c6656bc807b99a4
Jayleh <jaylehyun@gmail.com>
2019-03-29 22:21:50 -0700

    adding more language to services

--- client/src/components/serviceDetail/serviceDetailContent.js
+++ client/src/components/serviceDetail/serviceDetailContent.js
@@ -2,10 +2,9 @@ export default [
   {
     service: 'Hair Styling',
     description:
-      'With over 20 years of salon service and experience, our cosmetologists ' +
-      'are ready to meet all your hair needs! Whether you’re looking to pamper ' +
-      "yourself or find your next go-to salon at affordable pricing, we've got " +
-      'it all.',
+      'With over 20 years of salon service and experience, our cosmetologists are ready to meet ' +
+      'all your hair needs! Whether you’re looking to pamper yourself or find your next go-to ' +
+      "salon at affordable pricing, we've got it all.",
     subservices: [
       {
         subservice: 'Cut',
@@ -24,11 +23,10 @@ export default [
       }
     ],
     subDescription:
-      'Haircuts are available for all members of the family and the list of ' +
-      'services is endless: perms, coloring, Brazilian Blow-Out Treatment, ' +
-      'Japanese Permanent Straightening, hair extensions, and balayage just to ' +
-      'name a few. Find a simple, comprehensive list of pricing in the Prices ' +
-      'tab.',
+      'Haircuts are available for all members of the family and the list of services is ' +
+      'endless: perms, coloring, Brazilian Blow-Out Treatment, Japanese Permanent ' +
+      'Straightening, hair extensions, and balayage just to name a few. Find a simple, ' +
+      'comprehensive list of pricing in the Prices tab.',
     images: [
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' },
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' },
@@ -39,10 +37,9 @@ export default [
   {
     service: 'Nails Care',
     description:
-      'At New York Styles Salon & Spa, pamper yourself and get the star ' +
-      "treatment you deserve. Relax in our pedicure massage chairs and we'll " +
-      'do the rest. Have a design in mind? Bring it in or show us on your ' +
-      'phone! We also have hundreds of designs to choose from, so the ' +
+      'At New York Styles Salon & Spa, pamper yourself and get the star treatment you deserve. ' +
+      "Relax in our pedicure massage chairs and we'll do the rest. Have a design in mind? Bring " +
+      'it in or show us on your phone! We also have hundreds of designs to choose from, so the ' +
       'possibilities are endless.',
     subservices: [
       {
@@ -54,88 +51,106 @@ export default [
         subservice: 'Deluxe Pedicure',
         price: '$36',
         detail:
-          'Includes pedicure and callus treatment, exfoliating massage with sea salt scrub, mask or paraffin, cooling gel, and cucumber heel therapy.'
+          'Includes pedicure and callus treatment, exfoliating massage with sea salt scrub, ' +
+          'mask or paraffin, cooling gel, and cucumber heel therapy.'
       },
       {
         subservice: 'Royal Pedicure',
         price: '$50',
         detail:
-          'Includes pedicure and callus treatment, exfoliating message with seal salt scrub, mask or paraffin, cooling gel, cucumber heel therapy, and 20 min. massage with warm '
+          'Includes pedicure and callus treatment, exfoliating message with seal salt scrub, ' +
+          'mask or paraffin, cooling gel, cucumber heel therapy, and 20 min. massage with warm '
       }
     ],
-    subDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    subDescription: '',
     images: [
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' },
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' }
     ]
   },
   {
-    service: 'Body Therapies',
+    service: 'Facial',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'Go from full, natural lashes to Va-Va-Voom, all in one place. Looking to minimize the ' +
+      'peach fuzz on your face or groom your brows to be ‘on-fleek’? You’ve come to the right ' +
+      'place! Our trained estheticians are excited to meet all your facial needs.',
     subservices: [
       {
         subservice: 'European Facial',
         price: '$90',
         detail:
-          'Treatment includes deep cleansing, exfoliation of dull skin cells, extractions of impurities, and massage of face & shoulders. Mask for your skin type. '
+          'Treatment includes deep cleansing, exfoliation of dull skin cells, extractions ' +
+          'of impurities, and massage of face & shoulders. Mask for your skin type.'
       },
       {
         subservice: 'Brightening',
         price: '$90',
-        detail:
-          'Stop uneven pigmentation. Combined with proper homecare, regulates pigment production on a cellular level, helping to balance complexion. '
+        detail: ''
       },
       {
         subservice: 'Medicating Clearing',
         price: '$90',
-        detail:
-          'Breakouts have finally met their match. Our medicated clearing treatment will focus on purifying your skin with deep cleansing followed by extractions (For face or back).'
+        detail: ''
       },
       {
         subservice: 'Multivitamin Power ',
         price: '$90',
-        detail:
-          'For the latest in vitamin repair and hydroxyl acid exfoliation, try our most popular treatment. Designed to treat prematurely aging skin conditions, this treatment includes a non-irritating vitamin power exfoliation and a vitamin recovery mask to noticeably improve skin elasticity, tone, and texture.'
+        detail: ''
       },
       {
         subservice: 'Environmental Control',
         price: '$90',
-        detail:
-          'If your skin is looking red, inflamed, or feeling itchy, then this is the treatment for you! Our super soothing cleansing will remove impurities from your skin, followed by a calming oatmeal mask that will instantly calm redness and reduce inflammation. '
+        detail: ''
       }
     ],
     subDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      '*Yes, we also do special occasion makeup! Please schedule a consultation for Makeup ' +
+      'Services, so that we may best fit your needs.',
     images: [
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' },
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' }
     ]
   },
   {
-    service: 'Waxing',
+    service: 'Spa',
     description:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      'Hit the reset button and treat your skin right. Choose from services that brighten your ' +
+      'skin, revitalize tired eyes, or simply relax with a massage. ',
     subservices: [
       {
-        subservice: 'Brazilian',
-        price: '$',
-        detail: ''
+        subservice: 'Brightening',
+        price: '$90',
+        detail:
+          'Stop uneven pigmentation. Combined with proper homecare, regulates pigment ' +
+          'production on a cellular level, helping to balance complexion.'
       },
       {
-        subservice: 'Legs',
-        price: '$',
-        detail: ''
+        subservice: 'Medicating Clearing',
+        price: '$90',
+        detail:
+          'Breakouts have finally met their match. Our medicated clearing treatment will focus ' +
+          'on purifying your skin with deep cleansing followed by extractions (For face or back).'
       },
       {
-        subservice: 'Bikini',
-        price: '$',
-        detail: ''
+        subservice: 'Multivitamin Power ',
+        price: '$90',
+        detail:
+          'For the latest in vitamin repair and hydroxyl acid exfoliation, try our most popular ' +
+          'treatment. Designed to treat prematurely aging skin conditions, this treatment ' +
+          'includes a non-irritating vitamin power exfoliation and a vitamin recovery mask to ' +
+          'noticeably improve skin elasticity, tone, and texture.'
+      },
+      {
+        subservice: 'Environmental Control',
+        price: '$90',
+        detail:
+          'If your skin is looking red, inflamed, or feeling itchy, then this is the treatment ' +
+          'for you! Our super soothing cleansing will remove impurities from your skin, ' +
+          'followed by a calming oatmeal mask that will instantly calm redness and reduce ' +
+          'inflammation.'
       }
     ],
-    subDescription:
-      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    subDescription: '',
     images: [
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' },
       { imgSrc: 'https://picsum.photos/200/?random', imgAlt: '' }
